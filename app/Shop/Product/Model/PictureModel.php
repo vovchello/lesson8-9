f1bacac60f2cbca53ff162a76e315340c94fe6a9
vovchello_10 <vovka9379@gmail.com>
2018-10-10 11:21:23 +0300

    add images into the product

---
+++ app/Shop/Product/Model/PictureModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 09.10.18
+ * Time: 13:45
+ */
+
+namespace App\Shop\Product\Model;
+
+
+class PictureModel
+{
+
+}
---
+++ app/Shop/Product/Repository/PictureRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 09.10.18
+ * Time: 13:48
+ */
+
+namespace App\Shop\Product\Repository;
+
+
+class PictureRepository
+{
+
+}
